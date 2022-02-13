4f871b634cfccc5184767b02a2f6f0df717d4495
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-13 10:20:28 +0600

    eeeeh commit

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-console.log('I am in a separate file')
+// console.log('I am in a separate file')
+
+function updateCaseNumber(isIncreasing){
+    const caseInputText=document.getElementById('case-number');
+
+    let  caseNumber=caseInputText.value;
+    if(isIncreasing==true){
+        caseNumber=parseInt(caseNumber)+1;
+    }
+    else if(caseNumber>0){
+        caseNumber=parseInt(caseNumber)-1
+    }
+caseInputText.value=caseNumber;
+//update case price
+const caseTotal=document.getElementById('case-price');
+caseTotal.innerText=caseNumber*59;
+
+    // console.log(caseNumber);
+}
 
 document.getElementById('case-plus').addEventListener('click',function(){
 
-const caseInputText=document.getElementById('case-number');
+updateCaseNumber(true);
 
-const caseNumber=caseInputText.value;
-caseInputText.value=parseInt(caseNumber)+1;
-// console.log(caseNumber);
+});
+document.getElementById('case-minus').addEventListener('click',function(){
 
+ updateCaseNumber(false)
+;
 })
