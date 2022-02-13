0440f46da3d4de446d6813b37e518691a19e373a
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-13 09:38:59 +0600

    efseffs commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('I am in a separate file')
+
+document.getElementById('case-plus').addEventListener('click',function(){
+
+const caseInputText=document.getElementById('case-number');
+
+const caseNumber=caseInputText.value;
+caseInputText.value=parseInt(caseNumber)+1;
+// console.log(caseNumber);
+
+})
